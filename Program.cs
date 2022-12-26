ce941201a525c4fee2957c0faf6ad367858b0e5a
Your Name <you@example.com>
2022-12-26 21:06:13 +0600

    Reshenie DZ

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет*/
+
+ bool isWeek(int day )
+{
+   if(day == 6 || day == 7 )
+        { 
+     return true;
+    }
+   else
+    {
+     return false;
+    }
+}
+Console.Write("Enter the day of the week: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if(isWeek(day))
+     {
+        Console.Write("Yes");
+     }
+else
+     {
+        Console.Write("No");
+     }
+     
